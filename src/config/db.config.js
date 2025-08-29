9cbeff65678a1df9307abbe2ef10ffb4fedf41d8
DGreegman <obeagug@gmail.com>
2025-08-29 16:55:52 +0100

    DB config modified

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -5,13 +5,14 @@ export const connectDB = async () => {
     const connection_string = process.env.MONGO_URI 
 
     if(!connection_string){
-        throw new Error(
-            "Connection String is not Set"
-        )
+        throw new Error("Connection String is not Set")
     }
 
     try {
-        const conn = await mongoose.connect(connection_string)
+        const conn = await mongoose.connect(connection_string, {
+            connectTimeoutMS: 60000, // Increase timeout to 60 seconds
+            socketTimeoutMS: 60000
+        })
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
         console.log(error)
